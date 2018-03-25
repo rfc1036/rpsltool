a744fa14174355848d50b20c319c57abe852c33f
Marco d'Itri <md@linux.it>
2018-03-25 11:48:46 +0200

    New backend: support bgpq3 along with the old Net::Whois::RIPE

---
+++ lib/BGPQ3.pm
@@ -0,0 +1,189 @@
+package BGPQ3;
+
+use Types::Standard qw(Bool Int Str Maybe ArrayRef InstanceOf);
+use IPC::System::Simple qw(capturex);
+
+use Moo;
+use namespace::clean;
+
+##############################################################################
+has path => (
+	is => 'rw',
+	isa => Str,
+	default => 'bgpq3',
+);
+
+has host => (
+	is => 'rw',
+	isa => Str,
+);
+
+has port => (
+	is => 'rw',
+	isa => Int,
+);
+
+has asn32 => (
+	is => 'rw',
+	isa => Bool,
+	default => 1,
+);
+
+has asdot => (
+	is => 'rw',
+	isa => Bool,
+);
+
+has ipv6 => (
+	is => 'rw',
+	isa => Bool,
+);
+
+has debug => (
+	is => 'rw',
+	isa => Bool,
+);
+
+has sources => (
+	is => 'rw',
+	isa => ArrayRef[Str],
+	# from Str to ArrayRef[Str]
+	coerce => sub { ref $_[0] ? $_[0] : [ split(/,/, $_[0]) ] },
+	predicate => 1,
+);
+
+has aspath_query => (
+	is => 'rw',
+	isa => Bool,
+);
+
+has whois_show_cache_misses => (
+	is => 'rw',
+	isa => Bool,
+	default => $ENV{WHOIS_SHOW_CACHE_MISSES} || 0,
+);
+
+has cache_root => (
+	is => 'rw',
+	isa => Maybe[Str],
+);
+
+has cache_depth => (
+	is => 'rw',
+	isa => Int,
+	default => 0,
+);
+
+has cache => (
+	is => 'rw',
+	isa => Maybe[InstanceOf['CHI', 'Cache::BaseCache']],
+);
+
+##############################################################################
+sub _bgpq3_cmdline {
+	my ($self, @objects) = @_;
+
+	my $aspath_query;
+
+	foreach (my $i = 0; $i <= $#objects; $i++) {
+		# determine is this is a query for as-paths or prefixes
+		if (not defined $self->aspath_query) {
+			if ($objects[$i] =~ s/^<(.+)>$/$1/) {
+				$aspath_query = 1;
+			} else {
+				$aspath_query = 0;
+			}
+		} elsif ($self->aspath_query == 0) {
+			die "Unsupported mixed query (@objects)!"
+				if @objects > 1 and $objects[$i] =~ /^</;
+			$aspath_query = 1 if $objects[$i] =~ s/^<(.+)>$/$1/;
+		} elsif ($self->aspath_query == 1) {
+			# <> are optional and useless if aspath_query is set
+			$objects[$i] =~ s/^<(.+)>$/$1/;
+		}
+	}
+
+	my @params;
+	push(@params, '-3') if $self->asn32;
+	push(@params, '-6') if $self->ipv6;
+	push(@params, '-d') if $self->debug;
+	push(@params, '-D') if $self->asdot;
+	push(@params, '-S', join(',', @{$self->sources}))
+		if $self->has_sources;
+	push(@params, '-h', $self->host . ($self->port ? ':' . $self->port : ''))
+		if $self->host;
+
+	if ($self->aspath_query or $aspath_query) {
+		push(@params, '-G', '1');		# as-path list
+	} else {
+		push(@params, '-F', '%n/%l\n');	# prefixes
+	}
+
+	return (@params, sort @objects)
+}
+
+sub query {
+	my ($self, @objects) = @_;
+
+	# dereference the list if needed
+	@objects = @{$objects[0]} if ref $objects[0] eq 'ARRAY';
+
+	my @cmdline = $self->_bgpq3_cmdline(@objects);
+
+	my $cache_key = join(' ', @cmdline);
+
+	if ($self->cache) {
+		my $cached = $self->cache->get($cache_key);
+		return wantarray ? @$cached : $cached if $cached;
+
+		# some debugging code to show when an object is not in cache
+		print STDERR "============> CACHE MISS: @objects\n"
+			if $self->whois_show_cache_misses;
+	}
+
+	print STDERR join(' ', 'QUERY:', $self->path, @cmdline) . "\n"
+		if $self->debug;
+
+	my @output = capturex($self->path, @cmdline);
+	chomp @output;
+
+	# remove any trailing empty lines
+	pop @output while @output and $output[$#output] eq '';
+
+	# extract just the list of ASNs
+	if (@output and $output[0] =~ /^no ip as-path access-list /) {
+		@output =
+			map { split(/\|/) }
+			grep { defined $_ }
+			map { /^ip as-path access-list \S+ permit \S+_\(([0-9\|]+)\)\$/; $1 }
+			@output;
+	}
+
+	$self->cache->set($cache_key, \@output) if $self->cache;
+
+	return wantarray ? @output : \@output;
+}
+
+sub routes {
+	my $self = shift;
+
+	my $saved = $self->aspath_query;
+	$self->aspath_query(0);
+	my $output = $self->query(@_);
+	$self->aspath_query($saved);
+
+	return wantarray ? @$output : $output;
+}
+
+sub aspath {
+	my $self = shift;
+
+	my $saved = $self->aspath_query;
+	$self->aspath_query(1);
+	my $output = $self->query(@_);
+	$self->aspath_query($saved);
+
+	return wantarray ? @$output : $output;
+}
+
+1;
---
+++ lib/RPSLToolBGPQ3.pm
@@ -0,0 +1,134 @@
+use warnings;
+use strict;
+
+use BGPQ3;
+use Cache::FileCache;
+
+# should be moved to a different module?
+sub bgpq3_factory {
+	my ($param) = @_;
+
+	my $bgpq3 = BGPQ3->new or die;
+	$bgpq3->host($param->{whois_server}) if $param->{whois_server};
+
+	$bgpq3->debug(1) if $param->{whois_debug};
+	$bgpq3->sources($param->{whois_source}) if $param->{whois_source};
+	$bgpq3->asn32($param->{asn32_supported})
+		if defined $param->{asn32_supported};
+
+	# setup the query cache
+	my $cache = new Cache::FileCache({
+		cache_root	=> $param->{cache_root},
+		cache_depth	=> ($param->{cache_depth} || 0),
+	}) if $param->{cache_root};
+	$bgpq3->cache($cache) if $cache;
+
+	return $bgpq3;
+}
+
+##############################################################################
+package BGPQ3;
+
+use warnings;
+use strict;
+
+my ($v6route, $v4route, $aut_num, $as_set, $route_set, $routes_range);
+
+$v6route		= qr/[:0-9a-fA-F\/^\-\+]+/;
+$v4route		= qr/[\.0-9\/^\-\+]+/;
+$aut_num		= qr/[Aa][Ss][0-9]+/;
+$as_set			= qr/(?:$aut_num:)?[Aa][Ss]-[:A-Za-z0-9_\-]+/;
+$route_set		= qr/(?:$aut_num:)?[Rr][Ss]-[:A-Za-z0-9_\-]+/;
+$routes_range	= qr/[0-9\+\-]+/;
+
+sub import {
+	my ($self, $import, $ipv6, $default_aspath_filter) = @_;
+
+	$import = [ $import ] if not ref $import;
+
+	$self->ipv6($ipv6);
+	my $saved_sources = $self->sources;
+
+	my (@routes, @aspathlist, $query_sources);
+	foreach (@$import) {
+		next if not defined $_;
+		# ask for an as-path instead of a list of routes
+		my $aspath_query = 1 if s/^<(.+)>$/$1/;
+
+		# augment the RPSL language by allowing to override the object
+		# source(s) by prefixing objects with "SOURCE,SOURCE::"
+		if (s/^([A-Z0-9,]+):://) {
+			$self->sources($1);
+		} elsif ($saved_sources) {
+			$self->sources($saved_sources);
+		}
+
+		if      (/^$aut_num$/o and $aspath_query) {
+			s/^AS//;
+			push(@aspathlist, $_);
+		} elsif (/^$as_set$/o and $aspath_query) {
+			push(@aspathlist, $self->aspath($_));
+		} elsif (/^$aut_num$/o) {
+			push(@routes, $self->query($_));
+			s/^AS//;
+			push(@aspathlist, $_) if $default_aspath_filter;
+		} elsif (/^$as_set$/o) {
+			push(@routes, $self->query($_));
+			push(@aspathlist, $self->aspath($_)) if $default_aspath_filter;
+		} elsif (/^$route_set/o) {
+			push(@routes, $self->query($_));
+		} elsif ($ipv6     and /^$v6route$/o) {
+			push(@routes, $_);
+		} elsif (not $ipv6 and /^$v4route$/o) {
+			push(@routes, $_);
+		} else {
+			die "cannot parse '$_'\n";
+		}
+	}
+
+	# restore the original value
+	$self->sources($saved_sources) if $saved_sources;
+
+	return (\@routes, \@aspathlist);
+}
+
+##############################################################################
+sub expand_as_set {
+	my ($self, $name) = @_;
+
+	# just return the argument if it is an aut-num
+	if ($name =~ /^$aut_num$/o) {
+		$name =~ s/^AS//;
+		return $name;
+	}
+
+	return map { s/^AS//; $_; } $self->aspath($name);
+}
+
+sub expand_route_set {
+	my ($self, $name, $ipv6) = @_;
+
+	# just return the argument if it is a route
+	return ($name) if $name =~ /^[\.:\da-fA-F\/^\-\+]+$/;
+
+	my $globalrange;
+	$globalrange = $2 if $name =~ s/^(.+)\^($routes_range)$/$1/o;
+
+	$self->ipv6($ipv6);
+	my @routes = $self->routes($name);
+
+	@routes = @{ rpsl_filter(\@routes, [ '0.0.0.0/0^' . $globalrange ]) }
+		if $globalrange;
+
+	return @routes;
+}
+
+##############################################################################
+sub asn_to_networks {
+	my ($self, $name, $ipv6) = @_;
+
+	$self->ipv6($ipv6);
+	return $self->routes($name);
+}
+
+1;
